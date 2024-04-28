74434f638e665f6917c0ed8e3fc5493b5cff4fc7
nielb-intlgr <niel@intelloger.com>
2024-04-28 13:35:43 +0200

    Updated stack struct definition

--- stack/stack.go
+++ stack/stack.go
@@ -1,41 +1,37 @@
 package stack
 
 type Stack struct {
-	Nodes []string
-	Pi    string // pi represents the parent node
-	Depth int
+	Values []string
 }
 
 func NewStack() *Stack {
 	return &Stack{
-		Nodes: make([]string, 0),
+		Values: make([]string, 0),
 	}
 }
 
 // Push adds an item to the top of the stack
-func (s *Stack) Push(values string, pi string, depth int) {
-	s.Nodes = append(s.Nodes, values)
-	s.Pi = pi
-	s.Depth = depth
+func (s *Stack) Push(value string) {
+	s.Values = append(s.Values, value)
 }
 
 // Pop removes an item from the top of the stack
 func (s *Stack) Pop() string {
-	length := len(s.Nodes)
+	length := len(s.Values)
 	// Extract the top item
-	toRemove := s.Nodes[length-1]
+	toRemove := s.Values[length-1]
 	// Remove the top item
-	s.Nodes = s.Nodes[:length-1]
+	s.Values = s.Values[:length-1]
 	return toRemove
 }
 
 // IsEmpty checks if the stack is empty
 func (s *Stack) IsEmpty() bool {
-	return len(s.Nodes) == 0
+	return len(s.Values) == 0
 }
 
 // Peek returns the item from the top of the stack without removing it
 func (s *Stack) Peek() string {
-	length := len(s.Nodes)
-	return s.Nodes[length-1]
+	length := len(s.Values)
+	return s.Values[length-1]
 }
